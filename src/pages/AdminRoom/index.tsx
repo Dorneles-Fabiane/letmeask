6da520c600cdd5c285cd197591172802a3036f48
Fabiane Dorneles <fabiane.mail.d@gmail.com>
2021-06-24 14:34:36 -0300

    Added stylization  and functionalities at 'like' button

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,59 +1,28 @@
-import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-import { database } from '../../services/firebase';
-import { useAuth } from '../../hooks/useAuth';
 
 import { Button } from '../../components/Button';
 import { RoomCode } from '../../components/RoomCode';
 import { Question } from '../../components/Question/index';
 
+import { useRoom } from '../../hooks/useRoom';
+//import { useAuth } from '../../hooks/useAuth';
+
 import logoImg from '../../assets/images/logo.svg';
 
 import './style.scss';
 
-import { useRoom } from '../../hooks/useRoom';
 
 type RoomParams = {
    id: string;
 }
 
 export function AdminRoom() {
-
-   const { user } = useAuth();
+   //const { user } = useAuth();
 
    const params = useParams<RoomParams>();
 
-   const [newQuestion, setNewQuestion] = useState(''); 
-
    const roomId = params.id;
    const { questions, title } = useRoom(roomId);
-
-   async function handleSendQuestion(event: FormEvent) {
-      event.preventDefault();
-
-      if (newQuestion.trim() === '') {
-         return;
-      }
-
-      if (!user) {
-         throw new Error('You must be logged in');
-      }
-
-      const question = {
-         content: newQuestion,
-         author: {
-            name: user.name,
-            avatar: user.avatar
-         },
-         isHighlighted: false,
-         isAnswered: false,
-      };
-
-      await database.ref(`rooms/${roomId}/questions`).push(question);
-
-      setNewQuestion('');
-   }
 
    return (
       <div id="page-room">
